0fd527b0190d96058793ccd9aeb065b336f8d532
Jeffrey S Varghese <93147448+jsv1604@users.noreply.github.com>
2023-02-15 22:40:46 +0530

    Create task5.cpp

---
+++ task5.cpp
@@ -0,0 +1,11 @@
+//File: task5.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, from task 5\n";
+  cout << "SRN: PES1UG20CS651\n";
+  cout << "Name: Jeffrey\n";
+
+  return 0;
+}
